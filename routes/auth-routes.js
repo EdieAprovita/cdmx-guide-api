85e318aecb5814dffb5acffbdb83034fc81e4a5b
Edgar Chavero <eacm316@gmail.com>
2021-05-24 20:28:07 -0500

    Fixing auth routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -11,17 +11,17 @@ const {
 	updateUser,
 } = require('../controllers/auth')
 
-const { protect, admin } = require('../middleware/authMiddleware')
+const { protect } = require('../middleware/authMiddleware')
 
 //AUTH ROUTES
 
-router.get('/', protect, admin, getUsers)
+router.get('/', protect, getUsers)
 router.get('/profile', protect, getUserProfile)
-router.get('/:id', protect, admin, getUserById)
+router.get('/:id', protect, getUserById)
 router.post('/signup', registerUser)
 router.post('/login', authUser)
 router.put('/profile/updateMe', protect, updateUserProfile)
-router.put('/update/:id', protect, admin, updateUser)
-router.delete('/delete/:id', protect, admin, deleteUser)
+router.put('/update/:id', protect, updateUser)
+router.delete('/delete/:id', protect, deleteUser)
 
 module.exports = router
